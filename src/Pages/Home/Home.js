b45aab557351e1939c8d435db44c590ba988e596
best-developer-in-africa <wilsonajikwu12@gmail.com>
2022-10-21 09:08:43 +0100

    done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -21,9 +21,9 @@ function Home() {
           <Link className="btn btn-primary btn-hover-dark w-100" to='#instructor' smooth >Click to join DelzyScholars Instructors</Link>
         </div>
       </div>
+      <ActionStart main='Purchase Delzy Study Materials' span=' (WAEC, JAMB, PUTME, UNIPORT, RSUST, IAUE, UNIUYO e.t.c.)' sub='Delzy Study Materials' btn='Purchase Now' link='#course' />
       <ActionStart main='Book for a private session/tutorial. Both online & offline' span=' (WAEC, JAMB, PUTME, UNIPORT, RSUST, IAUE, UNIUYO e.t.c.)' sub='Private Session' btn='Book Now' link='/private' />
       <ActionStart main="Let's help you write your" span='I.T REPORT, RESEARCH PROJECT & MASTER THESIS' sub='REPORT, PROJECT & THESIS' btn='Sign Up' link='/project' />
-      <ActionStart main='Purchase Delzy Study Materials' span=' (WAEC, JAMB, PUTME, UNIPORT, RSUST, IAUE, UNIUYO e.t.c.)' sub='Delzy Study Materials' btn='Purchase Now' link='#course' />
       <div className='section section-padding-02'>
         <div className='container'>
           <TopStartCourse />
